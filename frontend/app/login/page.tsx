576f94360f66bd1744f317f014560ec32ae39120
Risma Shatiya <rismashatiya04@gmail.com>
2026-04-26 22:06:29 +0700

    Delete frontend directory

--- frontend/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import LoginForm from "@/features/auth/login/components/LoginForm";
-
-export default function LoginPage() {
-    return <LoginForm />;
-}
--- frontend/app/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import RegisterForm from "@/features/auth/register/components/RegisterForm";
-
-export default function RegisterPage() {
-  return <RegisterForm />;
-}
